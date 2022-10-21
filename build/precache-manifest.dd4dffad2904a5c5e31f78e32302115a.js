48eddd4106e78a87675a08291dae2e9cf7813286
Emibrown <emekaekene@gmail.com>
2022-10-21 12:57:19 +0100

    routes updated

---
+++ build/precache-manifest.dd4dffad2904a5c5e31f78e32302115a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "574f972202a671483a46",
+    "url": "/static/css/main.82e3da69.chunk.css"
+  },
+  {
+    "revision": "574f972202a671483a46",
+    "url": "/static/js/main.574f9722.chunk.js"
+  },
+  {
+    "revision": "156921dc609b6f36a167",
+    "url": "/static/js/1.156921dc.chunk.js"
+  },
+  {
+    "revision": "b7d05203191befeda396",
+    "url": "/static/js/2.b7d05203.chunk.js"
+  },
+  {
+    "revision": "4698be83918cb30a80a5",
+    "url": "/static/js/runtime~main.4698be83.js"
+  },
+  {
+    "revision": "a41a02e44d4c8b606e1e36092661be18",
+    "url": "/static/media/logo.a41a02e4.svg"
+  },
+  {
+    "revision": "e8dad050d121beb8e7788f7e4ee74787",
+    "url": "/static/media/dig-art.e8dad050.svg"
+  },
+  {
+    "revision": "069aac628bc7517cabd6b307a1030e7e",
+    "url": "/static/media/avatar.069aac62.svg"
+  },
+  {
+    "revision": "09eaf947061c16b274912873a1800bbf",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cacaccf76c4261135a91470aebc6e7cd.js"
+  "/precache-manifest.dd4dffad2904a5c5e31f78e32302115a.js"
 );
 
 workbox.clientsClaim();
